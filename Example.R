1df3c47149f8650bcc1986ee5d4cef68a1dc3ba6
RenXiaonan <90027331+RenXiaonan@users.noreply.github.com>
2022-06-19 22:25:13 +0800

    Update Example.R

--- Example.R
+++ Example.R
@@ -6,7 +6,7 @@
 ##################### Call packages and functions #################
 
 library(MASS)
-source("~/PINFunc.R")
+source("~/PIN-GitHub/PINFunc.R")
 
 ######################### Homogeneity model ########################
 ### Generate simulated datasets:            
@@ -34,6 +34,7 @@ Lambda=0.00001 # tuning parameter for preventing overfitting in the objective fu
 
 # Each time 80% of the samples are randomly selected to fit the network and obtain important variables, repeated 10 times.
 eva=NULL
+pe_test=rep(0,10)
 
 s=1
 for (s in 1:10) {
@@ -52,6 +53,18 @@ for (s in 1:10) {
   index=ho_pin$index
   eva=rbind(eva,Eva(index,b1))
   
+  xtest=dataClean[[3]]
+  ytest=dataClean[[4]]
+  O_test=pre_pin(xtest,ho_pin)
+  
+  pe_test[s]=0
+  for(i in 1:length(xtest)){
+    for(j in 1:(dim(xtest[[i]])[1])){
+      pe_test[s]= pe_test[s]+(ytest[[i]][j]-O_test[[i]][j])^2
+    }
+  }
+  pe_test[s]=pe_test[s]/((n1+n2+n3)*0.2)
+
   s=s+1
   
 }
@@ -59,7 +72,7 @@ for (s in 1:10) {
 ho_result = matrix(apply(eva,2,mean),nrow = 1)
 colnames(ho_result) = c("SEN","SPE","GM","CCR")
 ho_result
-
+matrix(c(mean(pe_test),sqrt(var(pe_test))),nrow=1)
 
 ######################### Heterogeneity model ########################
 ### Generate simulated datasets:            
@@ -88,6 +101,7 @@ Lambda=0.00001 # tuning parameter for preventing overfitting in the objective fu
 eva1=NULL
 eva2=NULL
 eva3=NULL
+pe_test=rep(0,10)
 
 s=1
 for (s in 1:10) {
@@ -111,7 +125,17 @@ for (s in 1:10) {
   eva2=rbind(eva2,Eva(index2,b2))
   eva3=rbind(eva3,Eva(index3,b3))
   
-  print(rbind(Eva(index1,b1),Eva(index2,b2),Eva(index3,b3)))
+  xtest=dataClean[[3]]
+  ytest=dataClean[[4]]
+  O_test=pre_pin(xtest,he_pin)
+  
+  pe_test[s]=0
+  for(i in 1:length(xtest)){
+    for(j in 1:(dim(xtest[[i]])[1])){
+      pe_test[s]= pe_test[s]+(ytest[[i]][j]-O_test[[i]][j])^2
+    }
+  }
+  pe_test[s]=pe_test[s]/((n1+n2+n3)*0.2)
   
   s=s+1
   
@@ -125,3 +149,7 @@ he_result = rbind(he_result,matrix(apply(he_result,2,mean),nrow = 1))
 colnames(he_result) = c("SEN","SPE","GM","CCR")
 rownames(he_result) = c("data1","data2","data3","mean")
 he_result
+
+matrix(c(mean(pe_test),sqrt(var(pe_test))),nrow=1)
+
+
